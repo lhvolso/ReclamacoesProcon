4c1c26501c81b27b1ab85bcd175300e132a937bd
Luiz Henrique Volso <lhvolso@gmail.com>
2012-12-06 20:20:38 -0200

    Envios dos Primeiros Arquivos
    HTML/CSS ainda incompleto por ter faltado o layout da paginação

---
+++ html/index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Reclamações Procon - Consulte antes da sua próxima compra!</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta name="author" content="Group VOID">
+	<meta name="robots" content="noindex">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/layout.css">
+	<link href="http://fonts.googleapis.com/css?family=Maven+Pro:400,700" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<header>
+		<h1>Reclamações Procon</h1>
+		<form action="#">
+			<input type="text" name="busca" placeholder="Busque aqui a empresa, o assunto ou problema">
+			<button type="submit">Buscar</button>
+		</form>
+	</header>
+	<section class="centro">
+		<!--<a href="#" class="ajuda">Ajuda</a>-->
+		<p class="ajuda">As reclamações são divididas em Nome da Empresa, Assunto e Descrição do Problema, utilize os filtros à direita para refinar a busca.</p>
+		<p class="titulofiltros">Filtros:</p>
+		<ul class="filtros">
+			<li><a href="#" class="empresas">Empresas</a></li>
+			<li><a href="#" class="assuntos">Assuntos</a></li>
+			<li><a href="#" class="problemas">Problemas</a></li>
+		</ul>
+		<h2 class="numeros">Sua busca por MIMIMI retornou 10.000 resultados</h2>
+		<ul class="resultados">
+			<li>
+				<p class="empresas">Empresa</p>
+				<h3><a href="#">SUBMARINO/SHOPTIME/AMERICANAS.COM</a></h3>
+				<p>5.000 reclamações, em <a href="#">98 assuntos</a> diferentes</p>
+			</li>
+			<li>
+				<p class="assuntos">Assunto</p>
+				<h3><a href="#">Artigo de Leitura ( Formal, Jornal, Revista, Folheto, Livros, Etc. )</a></h3>
+				<p><a href="#">150 empresas</a> reclamadas com <a href="#">107 problemas</a> diferentes</p>
+			</li>
+			<li>
+				<p class="problemas">Problema</p>
+				<h3><a href="#">Artigo de Leitura ( Formal, Jornal, Revista, Folheto, Livros, Etc. )</a></h3>
+				<p><a href="#">150 empresas</a> reclamadas com <a href="#">107 assuntos</a> diferentes</p>
+			</li>
+			<li>
+				<p class="empresas">Empresa</p>
+				<h3><a href="#">SUBMARINO/SHOPTIME/AMERICANAS.COM</a></h3>
+				<p>5.000 reclamações, em <a href="#">98 assuntos</a> diferentes</p>
+			</li>
+			<li>
+				<p class="assuntos">Assunto</p>
+				<h3><a href="#">Artigo de Leitura ( Formal, Jornal, Revista, Folheto, Livros, Etc. )</a></h3>
+				<p><a href="#">150 empresas</a> reclamadas com <a href="#">107 problemas</a> diferentes</p>
+			</li>
+			<li>
+				<p class="problemas">Problema</p>
+				<h3><a href="#">Artigo de Leitura ( Formal, Jornal, Revista, Folheto, Livros, Etc. )</a></h3>
+				<p><a href="#">150 empresas</a> reclamadas com <a href="#">107 assuntos</a> diferentes</p>
+			</li>
+			<li>
+				<p class="empresas">Empresa</p>
+				<h3><a href="#">SUBMARINO/SHOPTIME/AMERICANAS.COM</a></h3>
+				<p>5.000 reclamações, em <a href="#">98 assuntos</a> diferentes</p>
+			</li>
+			<li>
+				<p class="assuntos">Assunto</p>
+				<h3><a href="#">Artigo de Leitura ( Formal, Jornal, Revista, Folheto, Livros, Etc. )</a></h3>
+				<p><a href="#">150 empresas</a> reclamadas com <a href="#">107 problemas</a> diferentes</p>
+			</li>
+			<li>
+				<p class="problemas">Problema</p>
+				<h3><a href="#">Artigo de Leitura ( Formal, Jornal, Revista, Folheto, Livros, Etc. )</a></h3>
+				<p><a href="#">150 empresas</a> reclamadas com <a href="#">107 assuntos</a> diferentes</p>
+			</li>
+			<li>
+				<p class="problemas">Problema</p>
+				<h3><a href="#">Artigo de Leitura ( Formal, Jornal, Revista, Folheto, Livros, Etc. )</a></h3>
+				<p><a href="#">150 empresas</a> reclamadas com <a href="#">107 assuntos</a> diferentes</p>
+			</li>
+		</ul>
+	</section>
+</body>
+</html>
